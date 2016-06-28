8780477fc43057f1fd37cd367794f526c164e282
Nawaf Alsufiani <nawafh1986@hotmail.com>
2016-06-28 13:05:04 -0700

    added method control
    it returns the string "it is working"

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -51,7 +51,14 @@ public class WebController {
 		// with the URL: http://localhost:8080/
 		return "OK";
 	}
-
+    
+	@RequestMapping(value = "/cs480/Nawaf", method = RequestMethod.GET)
+	String control() {
+		// You can replace this with other string,
+		// and run the application locally to check your changes
+		// with the URL: http://localhost:8080/
+		return "It is working";
+	}
 	/**
 	 * This is a simple example of how to use a data manager
 	 * to retrieve the data and return it as an HTTP response.
